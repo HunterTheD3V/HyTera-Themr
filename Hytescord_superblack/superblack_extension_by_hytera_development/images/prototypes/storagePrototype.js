0b45bed6b529c5427c99d6ced779ed5473e11a7d
Nicolas Ramos <95194980+HunterD3V@users.noreply.github.com>
2022-07-26 23:16:27 -0300

    Add files via upload
    started new superblack github

---
+++ Hytescord_superblack/superblack_extension_by_hytera_development/images/prototypes/storagePrototype.js
@@ -0,0 +1,15 @@
+/* "storage": {
+  "managed_schema": "schema.json"
+}
+*/
+
+// QUOTES:
+
+/* data will be stored in storageFiles folder and declared in manifest just like in the prototype shown above.
+data will be converted to browser cookies
+this data, however is for schema.json string data
+all schemas and datas are string-based object contents
+the application of new overrides inside data may not work if you don't redeclare in manifest
+*/
+
+// QUOTE END
